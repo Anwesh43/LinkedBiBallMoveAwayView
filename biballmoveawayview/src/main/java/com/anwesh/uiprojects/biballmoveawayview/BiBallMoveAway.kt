72c288ada5992c2b98b8c30c2c6b29681bddd278
Anwesh43 <anweshthecool0@gmail.com>
2019-07-29 09:40:23 +0530

    adding Canvas extension methods

--- biballmoveawayview/src/main/java/com/anwesh/uiprojects/biballmoveawayview/BiBallMoveAway.kt
+++ biballmoveawayview/src/main/java/com/anwesh/uiprojects/biballmoveawayview/BiBallMoveAway.kt
@@ -21,6 +21,7 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#283593")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Float = 3.3f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
@@ -31,3 +32,34 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawArcAndMovingBall(i : Int, sc : Float, startDeg : Float, deg : Float, size : Float, w : Float, paint : Paint) {
+    val r : Float = size / rFactor
+    save()
+    rotate(deg)
+    paint.style = Paint.Style.FILL
+    drawCircle(size + w * sc.divideScale(i, balls), 0f, r, paint)
+    restore()
+    paint.style = Paint.Style.STROKE
+    drawArc(RectF(-size, -size, size, size), startDeg, deg, false, paint)
+}
+
+fun Canvas.drawBBMANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    var start : Float = 0f
+    var deg : Float = 0f
+    for (j in 0..(balls - 1)) {
+        val sc1i : Float = sc1.divideScale(j, balls)
+        deg = 180f * sc1i
+        drawArcAndMovingBall(j, sc2, start, deg, size, (w / 2 - size), paint)
+        start = 180F * Math.floor(sc1i.toDouble()).toFloat()
+    }
+}
