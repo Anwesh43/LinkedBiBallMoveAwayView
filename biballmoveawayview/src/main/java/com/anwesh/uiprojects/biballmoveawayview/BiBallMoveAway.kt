e4783ede5cda0e1df7127c8fdbc07803df88808d
Anwesh43 <anweshthecool0@gmail.com>
2019-07-29 09:57:44 +0530

    adding biballmoveaway module

--- biballmoveawayview/src/main/java/com/anwesh/uiprojects/biballmoveawayview/BiBallMoveAway.kt
+++ biballmoveawayview/src/main/java/com/anwesh/uiprojects/biballmoveawayview/BiBallMoveAway.kt
@@ -63,3 +63,21 @@ fun Canvas.drawBBMANode(i : Int, scale : Float, paint : Paint) {
         start = 180F * Math.floor(sc1i.toDouble()).toFloat()
     }
 }
+
+class BiBallMoveAway(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
